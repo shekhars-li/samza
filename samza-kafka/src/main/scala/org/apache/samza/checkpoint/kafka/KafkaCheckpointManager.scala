0ce7fcdcec70388d47cf48d5f5ce0423fa560c6c
Boris S <boryas@apache.org>
2017-10-05 15:36:37 -0700

    SAMZA-1442: use systemAdmin.validateStream in KafkaCheckpointManager
    Author: Boris S <boryas@apache.org>
    
    Reviewers: Jacob Maes <jacob.maes@gmail.com>
    
    Closes #314 from sborya/CheckpointTopicValidation

--- samza-kafka/src/main/scala/org/apache/samza/checkpoint/kafka/KafkaCheckpointManager.scala
+++ samza-kafka/src/main/scala/org/apache/samza/checkpoint/kafka/KafkaCheckpointManager.scala
@@ -28,6 +28,7 @@ import org.apache.samza.checkpoint.{Checkpoint, CheckpointManager}
 import org.apache.samza.container.TaskName
 import org.apache.samza.serializers.CheckpointSerde
 import org.apache.samza.system.SystemStreamMetadata.SystemStreamPartitionMetadata
+import org.apache.samza.system.kafka.{KafkaSystemAdmin, KafkaStreamSpec}
 import org.apache.samza.system.{StreamSpec, SystemAdmin, _}
 import org.apache.samza.util._
 import org.apache.samza.{Partition, SamzaException}
@@ -248,9 +249,27 @@ class KafkaCheckpointManager(
     info("Done reading %s messages from checkpoint system:%s topic:%s" format(msgCount, systemName, checkpointTopic))
   }
 
-  def start {
-    kafkaUtil.createTopic(checkpointTopic, 1, replicationFactor, checkpointTopicProperties)
-    kafkaUtil.validateTopicPartitionCount(checkpointTopic, systemName, metadataStore, 1, failOnCheckpointValidation)
+  override def start {
+    val CHECKPOINT_STREAMID = "unused-temp-checkpoint-stream-id"
+    val spec = new KafkaStreamSpec(CHECKPOINT_STREAMID,
+                                   checkpointTopic, systemName, 1,
+                                   replicationFactor, checkpointTopicProperties)
+
+    info("About to create checkpoint stream: " + spec)
+    systemAdmin.createStream(spec)
+    info("Created checkpoint stream: " + spec)
+    try {
+      systemAdmin.validateStream(spec) // SPECIAL VALIDATION FOR CHECKPOINT. DO NOT FAIL IF failOnCheckpointValidation IS FALSE
+      info("Validated spec: " + spec)
+    } catch {
+      case e : StreamValidationException =>
+             if (failOnCheckpointValidation) {
+               throw e
+             } else {
+               warn("Checkpoint stream validation partially failed. Ignoring it because failOnCheckpointValidation=" + failOnCheckpointValidation)
+             }
+      case e1 : Exception => throw e1
+    }
   }
 
   def register(taskName: TaskName) {
